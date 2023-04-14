7d32be4d5dc38d5048a62405b88b1d420d696793
Gabriela Carniel <gabiicarniel@gmail.com>
2023-04-14 14:23:19 -0300

    criado component button e input

--- src/Components/Login/LoginForm.js
+++ src/Components/Login/LoginForm.js
@@ -1,30 +1,32 @@
 import React, { useState } from 'react'
 import { Link } from 'react-router-dom'
+import Input from '../Forms/Input'
+import Button from '../Forms/Button'
 
 const LoginForm = () => {
   const [username, setUsername] = useState('')
   const [password, setPassword] = useState('')
 
-  const handleSubmit = (event) => {
+  const handleSubmit = async (event) => {
     event.preventDefault()
-    fetch('https://dogsapi.origamid.dev/json/jwt-auth/v1/token', {
+    let response = await fetch('https://dogsapi.origamid.dev/json/jwt-auth/v1/token', {
       method: 'post',
       headers: {
         'Content-type': 'application/json'
       },
       body: JSON.stringify({ username, password })
-    }).then((response) => {
-      console.log(response)
-      return response.json()
-    }).then(json => console.log(json))
+    })
+    let json = await response.json()
+    return console.log(json)
   }
+
   return (
     <section>
       <h1>Login</h1>
       <form action="" onSubmit={handleSubmit}>
-        <input type="text" value={username} onChange={({ target }) => (setUsername(target.value))} />
-        <input type="text" value={password} onChange={({ target }) => (setPassword(target.value))} />
-        <button>Entrar</button>
+        <Input label='Usuário' type='text' name='username' />
+        <Input label='Senha' type='password' name='password' />
+        <Button>Entrar</Button>
       </form>
       <Link to='/login/criar'>Cadastro</Link>
     </section>
